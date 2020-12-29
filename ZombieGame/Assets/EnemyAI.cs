6eb3b90a3e8222ae411629b8100124bbe5c98af9
Siavash Eftekharifar <siavash.eftekharifar1991@gmail.com>
2020-12-28 21:54:39 -0500

    SetUpEnemyAI

---
+++ ZombieGame/Assets/EnemyAI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMesh;
+    float distanceToPlayer = Mathf.Infinity;
+
+    void Start() {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        ProcessDestanceToPlayer();
+    }
+
+    private void ProcessDestanceToPlayer() {
+        distanceToPlayer = Vector3.Distance(playerTransform.position, transform.position);
+        if (distanceToPlayer <= chaseRange) {
+            AttackPlayer();
+        }
+    }
+
+    private void AttackPlayer() {
+        navMesh.destination = playerTransform.position;
+    }
+
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
